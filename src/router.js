d2f31e090269c579fefc1c5e4d87be23023592db
DarrickF <Darrick_F@live.com>
2021-03-14 16:14:47 -0500

    Added Auth via firebase

--- src/router.js
+++ src/router.js
@@ -4,12 +4,19 @@ import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Login from './views/Login.vue'
 import Secret from './views/Secret.vue'
+import { isAuthenticated } from './helpers/useAuth'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/about', component: About },
   { path: '/login', component: Login },
-  { path: '/secret', component: Secret },
+  {
+    path: '/secret',
+    component: Secret,
+    beforeEnter: (to, from) => {
+      if (isAuthenticated.value) return true
+      return '/login'
+    },
+  },
 ]
 
 export const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,11 @@
-<template>Home page</template>
+<template>
+  <div class="mt-8">
+    <p class="text-3xl mx-auto text-center m-8 tracking-wider font-thin">
+      Welcome {{ user?.email || 'New User' }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { user } from '../helpers/useAuth'
+</script>
