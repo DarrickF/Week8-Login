276f028decad3d35f64c0ce1b0a43001904f3c13
DarrickF <Darrick_F@live.com>
2021-03-14 15:08:14 -0500

    added firebase to prev login proj

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="navbar mb-2 shadow-lg bg-content-800 text-content-100">
+  <div class="navbar mb-2 shadow-lg bg-coolGray-600 text-content-100">
     <div class="flex-1 px-2 mx-2">
-      <span class="text-lg font-bold"> LoginExample </span>
+      <span class="text-lg font-bold text-orange-600"> LoginExample </span>
     </div>
     <div class="flex-none flex px-2 mx-2">
       <div class="flex">
         <router-link
           to="/"
-          class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          class="btn btn-ghost btn-sm rounded-btn hover:text-content-100 font-bold text-orange-600"
         >
           Home
         </router-link>
         <router-link
           v-if="!isAuthenticated"
           to="/login"
-          class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          class="btn btn-ghost btn-sm rounded-btn hover:text-content-100 font-bold text-orange-600"
         >
           Login
         </router-link>
         <div v-else>
           <router-link
             to="/secret"
-            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100 font-bold text-orange-600"
           >
             Secret Page
           </router-link>
           <button
-            class="btn btn-error btn-sm rounded-btn hover:text-content-100"
+            class="btn btn-error btn-sm rounded-btn hover:text-content-100 font-bold text-orange-100"
             @click="logout"
           >
             Logout
--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Login from './views/Login.vue'
+import Secret from './views/Secret.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/login', component: Login },
+  { path: '/secret', component: Secret },
 ]
 
 export const router = createRouter({
