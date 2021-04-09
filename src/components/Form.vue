ace9c1657fd5214c2b55ac08bd85a814255ef3bb
DarrickF <Darrick_F@live.com>
2021-04-09 13:08:43 -0500

    google sign in implementation

---
+++ src/components/Form.vue
@@ -0,0 +1,88 @@
+<template>
+  <section
+    class="container flex item-center justify-center text-center bg-blueGray-500 rounded-xl mx-auto mt-24 max-w-md"
+  >
+    <form @submit.prevent class="flex flex-col w-full p-12 rounded shadow-lg">
+      <label
+        for="username"
+        class="self-start text-xs font-semibold text-content-200"
+        >Email</label
+      >
+      <input
+        id="username"
+        type="text"
+        class="flex items-center h-12 px-4 mt-2 bg-gray-300 rounded focus:outline-none focus:ring-2"
+        v-model="email"
+      />
+      <label
+        for="password"
+        class="self-start mt-3 text-xs font-semibold text-content-200"
+        >Password</label
+      >
+      <input
+        id="password"
+        type="password"
+        class="flex items-center h-12 px-4 mt-2 bg-gray-300 rounded focus:outline-none focus:ring-2"
+        v-model="password"
+      />
+      <button
+        v-if="form == 'login'"
+        @click="login"
+        class="btn btn-primary mt-4 bg-violet-800 hover:bg-violet-900"
+      >
+        Login
+      </button>
+      <button
+        v-else
+        @click="register"
+        class="btn btn-primary mt-4 bg-violet-800 hover:bg-violet-900"
+      >
+        SignUp
+      </button>
+      <button @click="google" class="btn btn-primary-content mt-8">
+        <img class="w-8" src="../assets/google.svg" alt="Google Logo" />
+      </button>
+    </form>
+  </section>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue'
+import { useRouter } from 'vue-router'
+import { signIn, signUp, googlePopup } from '../helpers/useAuth'
+const router = useRouter()
+const login = async () => {
+  try {
+    await signIn(email.value, password.value)
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+const register = async () => {
+  try {
+    await signUp(email.value, password.value)
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const google = async () => {
+  try {
+    await googlePopup()
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const email = ref('')
+const password = ref('')
+defineProps({
+  form: {
+    type: String,
+    default: 'Login',
+  },
+})
+</script>
